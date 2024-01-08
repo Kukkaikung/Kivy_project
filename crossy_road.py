7e7c6f17ad84f7142121b1330f5062ce6201d779
Duriya Gosonhemmanee <duriya.0577@gmail.com>
2024-01-09 01:36:59 +0700

    add setting button and setting page and audio button

--- crossy_road.py
+++ crossy_road.py
@@ -6,18 +6,37 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
+from kivy.uix.scatter import Scatter
+from kivy.core.window import Window
 
 
 class MyApp(App) :
     
-    def build(self):
-        float_layout = FloatLayout()
-        label01 = Label(text = 'Hello This test Program', font_size = 50)
-        
-        float_layout.add_widget(label01)
+    def build(self) :
+        self.name_input = TextInput(text='Enter your name', font_size = 40)
+        start_button = Button(text='Start', font_size = 40, on_press=self.switch_to_start)
+        setting_button = Button(text='Setting', font_size = 40, on_press=self.switch_to_setting)
 
-        return float_layout
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(self.name_input)
+        layout.add_widget(start_button)
+        layout.add_widget(setting_button)
+
+        return layout
     
+    def switch_to_start(self, instance):
+        self.root.clear_widgets()
+        self.root.add_widget(Label(text=f"Game is starting, {self.name_input.text}", font_size = 40))
+
+    def switch_to_setting(self, instance):
+        self.root.clear_widgets()
+        audio_button = Button(text='Audio Setting')
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(audio_button)
+        self.root.add_widget(layout)
+
+    
+
 if __name__ == '__main__' :
     MyApp().run()
 
