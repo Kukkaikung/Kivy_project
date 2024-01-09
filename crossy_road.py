abb6e49212e11e51115eccbe4fc9960a3e275254
Duriya Gosonhemmanee <duriya.0577@gmail.com>
2024-01-09 16:57:26 +0700

    Change BoxLayout to GridLayout

--- crossy_road.py
+++ crossy_road.py
@@ -12,62 +12,62 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 
-class MainScreen(BoxLayout) :
-    def __init__(self, **kwargs) :
+class MainScreen(GridLayout):
+    def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
-        self.orientation = 'vertical'
+        self.cols = 1
 
-        self.name_input = TextInput(hint_text='Enter your name', font_size = 40)
-        self.confirm_button = Button(text = 'Confirm', font_size = 35, on_press = self.comfirm_pressed)
-        self.start_button = Button(text='Start', font_size = 35, on_press=self.start_game)
-        self.setting_button = Button(text='Setting',font_size = 35, on_press=self.go_to_settings)
+        self.name_input = TextInput(hint_text='Enter your name', font_size=40)
+        self.confirm_button = Button(text='Confirm', font_size=35, on_press=self.confirm_pressed)
+        self.start_button = Button(text='Start', font_size=35, on_press=self.start_game)
+        self.setting_button = Button(text='Setting', font_size=35, on_press=self.go_to_settings)
 
         self.add_widget(self.name_input)
         self.add_widget(self.confirm_button)
         self.add_widget(self.start_button)
         self.add_widget(self.setting_button)
 
-    def start_game(self, instance) :
+    def start_game(self, instance):
         MyApp.get_running_app().set_name(self.name_input.text)
         MyApp.get_running_app().screen_manager.current = 'game_screen'
-        
-    def go_to_settings(self, instance) :
+
+    def go_to_settings(self, instance):
         MyApp.get_running_app().screen_manager.current = 'settings_screen'
 
-    def comfirm_pressed(self, instance) :
+    def confirm_pressed(self, instance):
         name = self.name_input.text
-        your_name = Label(text = f'Your name {name}')
+        your_name = Label(text=f'Your name {name}')
 
 
-class GameScreen(BoxLayout) :
-    def __init__(self, **kwargs) :
+class GameScreen(GridLayout):
+    def __init__(self, **kwargs):
         super(GameScreen, self).__init__(**kwargs)
-        self.orientation = 'vertical'
-        self.name_label = Label(text = '')
+        self.cols = 1
+        self.name_label = Label(text='')
+
         self.add_widget(self.name_label)
-    
-    def set_name(self, name) :
+
+    def set_name(self, name):
         self.name_label.text = f'Your name: {name}'
 
 
-class SettingsScreen(BoxLayout) :
-    def __init__(self, **kwargs) :
+class SettingsScreen(GridLayout):
+    def __init__(self, **kwargs):
         super(SettingsScreen, self).__init__(**kwargs)
-        self.orientation = 'vertical'
-        
+        self.cols = 1
+
         self.audio_button = Button(text='Audio', on_press=self.audio_settings)
         self.back_button = Button(text='Back', on_press=self.go_back)
 
         self.add_widget(self.audio_button)
         self.add_widget(self.back_button)
 
-    def audio_settings(self, instance) :
+    def audio_settings(self, instance):
         # Add audio settings logic here
         pass
 
-    def go_back(self, instance) :
+    def go_back(self, instance):
         MyApp.get_running_app().screen_manager.current = 'main_screen'
-
 
 
 class MyApp(App):
@@ -90,8 +90,8 @@ class MyApp(App):
         self.screen_manager.add_widget(screen)
 
         return self.screen_manager
-    
-    def set_name(self, name) :
+
+    def set_name(self, name):
         self.game_screen.set_name(name)
 
 
